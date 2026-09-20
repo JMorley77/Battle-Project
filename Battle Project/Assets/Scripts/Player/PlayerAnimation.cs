51ba487b0ce68d329a86e2cfdfc04c4479291326
DESKTOP-OG6GRRU\James <James.Morley77@outlook.com>
2026-09-20 20:30:34 +0100

    Minor tweaks to animations, speed variables and bug fixing

--- Battle Project/Assets/Scripts/Player/PlayerAnimation.cs
+++ Battle Project/Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,41 +1,46 @@
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private PlayerCombat playerCombat;
     [SerializeField] private PlayerPickUp playerPickUp;
+
     public bool attackAnimationPlaying;
     private float targetSpeed;
+    private int lastPlayedCombo = 0;
 
     private void Update()
     {
-
-
         animator.SetFloat("Speed", targetSpeed, 0.1f, Time.deltaTime);
-
         animator.SetBool("IsJumping", !playerMovement.isGrounded);
-
-        if (playerPickUp.isHolding)
-        {
-            animator.SetBool("IsPickingUp", true);
-        }
-        else
-        {
-            animator.SetBool("IsPickingUp", false);
-        }
-
+        animator.SetBool("IsPickingUp", playerPickUp.isHolding);
 
         if (playerPickUp.equippedWeapon == null && attackAnimationPlaying)
         {
             StopAttackAnimation();
         }
 
+        // play next animation whenever currentCombo increases to a new step
+        if (playerCombat.isAttacking && playerCombat.currentCombo != lastPlayedCombo)
+        {
+            attackAnimationPlaying = true;
+            lastPlayedCombo = playerCombat.currentCombo;
+            ComboAnimation();
+        }
 
-        #region Movement Speed Animation Blend Tree
+        if (playerCombat.isDead)
+        {
+            DeathAnimation();
+        }
+
+        Movement();
+    }
+
+    #region Movement
+    public void Movement()
+    {
         if (attackAnimationPlaying)
         {
             targetSpeed = 0.6f;
@@ -49,69 +54,57 @@ public class PlayerAnimation : MonoBehaviour
         {
             targetSpeed = 0f;
         }
-        #endregion
 
-        #region Attack Animation
-        //play animation if the player is attacking and the attack animation is not already playing
-        if (playerCombat.isAttacking && !attackAnimationPlaying) // the !attackanimationplaying plays 1 animaiton not the combo
-        {
-            attackAnimationPlaying = true;
-            ComboAnimation();
-        }
-        #endregion
-
-
-        #region Death Animation
-        if (playerCombat.isDead)
-        {
-            DeathAnimation();
-        }
-        #endregion
     }
-    #region Death Animation 
+    #endregion
+
+    #region Attack Animation
+    public void ComboAnimation()
+    {
+        switch (playerCombat.currentCombo)
+        {
+            case 1:
+                animator.SetTrigger("IsAttacking");
+                break;
+            case 2:
+                animator.SetTrigger("Attack2");
+                break;
+            case 3:
+                animator.SetTrigger("Attack3");
+                break;
+        }
+    }
+    #endregion
+
+    #region Attack Animation Finish
+    // used in animation event at the end of the swing clip
+    public void AttackFinished()
+    {
+        attackAnimationPlaying = false;
+        playerCombat.isAttacking = false;
+        playerCombat.currentCombo = 0; // Reset combo count in combat script
+        lastPlayedCombo = 0;
+    }
+
+    public void StopAttackAnimation()
+    {
+        attackAnimationPlaying = false;
+        playerCombat.isAttacking = false;
+        playerCombat.currentCombo = 0;
+        lastPlayedCombo = 0;
+
+        playerMovement.sprintSpeed = playerMovement.origionalSprintSpeed;
+
+        animator.ResetTrigger("IsAttacking");
+        animator.ResetTrigger("Attack2");
+        animator.ResetTrigger("Attack3");
+    }
+    #endregion
+
+    #region Death Animation
     public void DeathAnimation()
     {
         animator.SetTrigger("IsDead");
     }
     #endregion
-
-    public void ComboAnimation() 
-    {
-        switch(playerCombat.currentCombo)
-        {
-            case 1:
-                animator.SetTrigger("IsAttacking");
-                AttackFinished();
-                break;
-            case 2:
-                animator.SetTrigger("Attack2");
-                AttackFinished();
-                break;
-            case 3:
-                animator.SetTrigger("Attack3");
-                AttackFinished();
-                break;
-            default:
-                break;
-        }
-    }
-    public void AttackFinished()
-    {
-        attackAnimationPlaying = false;
-        playerCombat.isAttacking = false; 
-        Debug.Log("Attack animation finished");
-    }
-
-    public void StopAttackAnimation()
-    {
-        if (!attackAnimationPlaying)
-            return;
-        attackAnimationPlaying = false;
-        playerCombat.isAttacking = false;
-        playerMovement.sprintSpeed = playerMovement.origionalSprintSpeed;
-        animator.ResetTrigger("IsAttacking");
-        animator.ResetTrigger("Attack2");
-        animator.ResetTrigger("Attack3");
-    }
-    
 }
--- Battle Project/Assets/Scripts/Player/PlayerCombat.cs
+++ Battle Project/Assets/Scripts/Player/PlayerCombat.cs
@@ -32,7 +32,7 @@ public class PlayerCombat : MonoBehaviour
 
     public bool isAttacking = false;
     public bool isDead = false;
-    public bool canCombo = false;
+    //public bool canCombo = false;
 
     void Update()
     {
